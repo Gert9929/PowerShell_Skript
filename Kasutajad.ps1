65c4f7f99938b59b25b48cf533a8ea9e38c9394e
Gert9929 <74728759+Gert9929@users.noreply.github.com>
2022-11-25 13:14:46 +0200

    Update Kasutajad.ps1

--- Kasutajad.ps1
+++ Kasutajad.ps1
@@ -1,7 +1,13 @@
 $Ees = Read-Host -Prompt "Sisestage kasutaja nimi"
 $Pere = Read-Host -Prompt "Sisestage kasutaja perekonna nimi"
-$Password = ConvertTo-SecureString "Parool1!" -AsPlainText -Force
+$Password = ConvertTo-SecureString "Parool1!" -AsPlainText -Force)
 $Kogunimi = $Ees.ToLower()+"."+$Pere.ToLower()
 echo $Kogunimi
 
-New-LocalUser -Name "$Ees" -Password $Password -FullName "Kogunimi" -Description "Uus user"  
+$GetUser = Get-LocalUser | where-Object Name -eq "$Ees" | Measure
+if ($GetUser.Count -eq 0) {
+     New-LocalUser  -Name "$Ees" -Password $Password -FullName "$Kogunimi"
+     echo "Kasutaja $Kogunimi on lisatud!"
+} else {
+     Write-Output "Kasutaja $Kogunimi juba eksisteerib"
+}
